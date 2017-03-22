ac96f41b5492434fc57d043b1998c93a41199a22
Thushara Sathkumara <smtsampath@Thusharas-MacBook-Pro.local>
2017-03-22 02:25:50 -0400

    Null Props

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = { term: '' };
+    this.state = { term: '' };
 
-        this.handleInputChange = this.handleInputChange.bind(this);
-    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
 
-    handleInputChange(event) {
-        this.setState({ term: event.target.value })
-    };
+  handleInputChange(event) {
+    this.setState({ term: event.target.value })
+  };
 
-    render() {
-        return (
-          <div>
-            <input
-              value={this.state.term}
-              onChange={this.handleInputChange} />
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={this.handleInputChange} />
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,32 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
-// import SideBar from './components/sidebar';
-// import PageContent from './components/page_content';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDPjqiFBZJpm4LGDzWJxV08O1T5mBjqt6Q';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = { videos: [] };
+    this.state = { videos: [] };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos }); // this.setState({ videos: videos });
-        });
-    }
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        this.setState({ videos }); // this.setState({ videos: videos });
+    });
+  }
 
-    render() {
-        return (
-          <div className="row">
-              {/* <SideBar />
-              <PageContent /> */}
-            <SearchBar />
-            <VideoList videos={this.state.videos} />
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div className="row">
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
